1d31f129e3955c70e031a0ed24cbc2ebdeea502f
SajibBarua-art <sajibbaruanstu@gmail.com>
2024-07-15 13:34:03 +0600

    first

--- src/main/java/com/example/jpamapping/entity/Customer.java
+++ src/main/java/com/example/jpamapping/entity/Customer.java
@@ -1,4 +1,23 @@
 package com.example.jpamapping.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String email;
+    private String gender;
 }
--- src/main/java/com/example/jpamapping/entity/Product.java
+++ src/main/java/com/example/jpamapping/entity/Product.java
@@ -1,4 +1,8 @@
 package com.example.jpamapping.entity;
 
 public class Product {
+    private int pid;
+    private String productName;
+    private int quantity;
+    private int price;
 }
